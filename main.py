065ab6b3795bf7680028214ecc5fde31f3f7f1a5
pjerzyna <pawel.jerzyna@interia.pl>
2023-11-30 12:11:21 +0100

    tests

--- main.py
+++ main.py
@@ -49,8 +49,3 @@ def stats(vector: Union[np.ndarray, list]):
         table.add_row([key, value])
     
     return table
-
-#the weather
-v = [15, 13, 15, 13, 11, 11, 12, 11, 11, 9, 10, 10 ,11, 10, 12, 10, 8, 8, 7, 6, 8, 6, 6, 7, 6, 0, 0, -1, -2, -1]
-m = stats(v)
-print(m)
