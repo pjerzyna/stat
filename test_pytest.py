6b5c128f4af7770cbba6fa7617a77733195ca2e5
pjerzyna <pawel.jerzyna@interia.pl>
2023-12-01 14:35:57 +0100

    configurating pytest

--- test_pytest.py
+++ test_pytest.py
@@ -1,5 +1,56 @@
-from main import stats
+from main import stats #importing a function to test
 import pytest
 
 
-# coming soon 
+# works well when it has one input and one output
+@pytest.mark.parametrize(
+        
+        ('input_vec', 'expected_output'),
+        (
+            ([1, 2, 3, 4, 5, 6], (
+"+--------------------------+---------------------+"
+"|   Statistical feature    |        Value        |"
+"+--------------------------+---------------------+"
+"|           mean           |         3.5         |"
+"|    standard deviation    |  1.707825127659933  |"
+"| coefficient of variation |       48.80 %       |"
+"|         minimum          |          1          |"
+"|       10 precentil       |         1.5         |"
+"|        1 quartile        |         2.25        |"
+"|         mediana          |         3.5         |"
+"|        3 quartile        |         4.75        |"
+"|      90 precentile       |         5.5         |"
+"|         maximum          |          6          |"
+"|        data range        |          5          |"
+"|   interquartile range    |         2.5         |"
+"|         skewness         |         0.0         |"
+"|         kurtosis         | -1.2685714285714282 |"
+"+--------------------------+---------------------+"
+    )),
+            ([6, 5, 4, 3, 2, 1], (
+"+--------------------------+---------------------+"
+"|   Statistical feature    |        Value        |"
+"+--------------------------+---------------------+"
+"|           mean           |         3.5         |"
+"|    standard deviation    |  1.707825127659933  |"
+"| coefficient of variation |       48.80 %       |"
+"|         minimum          |          1          |"
+"|       10 precentil       |         1.5         |"
+"|        1 quartile        |         2.25        |"
+"|         mediana          |         3.5         |"
+"|        3 quartile        |         4.75        |"
+"|      90 precentile       |         5.5         |"
+"|         maximum          |          6          |"
+"|        data range        |          5          |"
+"|   interquartile range    |         2.5         |"
+"|         skewness         |         0.0         |"
+"|         kurtosis         | -1.2685714285714282 |"
+"+--------------------------+---------------------+"
+    )),
+        )
+)
+
+
+
+def test_vector(input_vec, expected_output):
+    assert stats.vector(input_vec) == expected_output # function.test_name
