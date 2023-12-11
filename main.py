8d0e22d30aa9c528026941f3e0f0508e6c423a08
pjerzyna <pawel.jerzyna@interia.pl>
2023-12-11 14:22:48 +0100

    works well but with setup fun doesnt

--- main.py
+++ main.py
@@ -57,3 +57,7 @@ def stats(vector: Union[np.ndarray, list]) -> PrettyTable:
         table.add_row([key, value])
     
     return table
+
+v = [15, 13, 15, 13, 11, 11, 12, 11, 11, 9, 10, 10 ,11, 10, 12, 10, 8, 8, 7, 6, 8, 6, 6, 7, 6, 0, 0, -1, -2, -1]
+a = stats(v)
+print(a)
